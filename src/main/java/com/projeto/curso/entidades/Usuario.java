ab496891614ee9600bb096cf520728a7b8901a89
Matheus Santos <analprojeto@gmail.com>
2019-11-30 02:15:29 -0400

    Pedido, Instant, ISO 8601

--- src/main/java/com/projeto/curso/entidades/Usuario.java
+++ src/main/java/com/projeto/curso/entidades/Usuario.java
@@ -1,13 +1,20 @@
 package com.projeto.curso.entidades;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_usuario")
 public class Usuario implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +26,10 @@ public class Usuario implements Serializable{
 	private String email;
 	private String telefone;
 	private String senha;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Usuario() {}
 
@@ -70,6 +81,10 @@ public class Usuario implements Serializable{
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -95,7 +110,5 @@ public class Usuario implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
---
+++ src/main/java/com/projeto/curso/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.projeto.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.curso.entidades.Pedido;
+import com.projeto.curso.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repository;
+	
+	public List<Pedido> findAll(){
+		return repository.findAll();
+	}
+	
+	public Pedido findById(Long id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
